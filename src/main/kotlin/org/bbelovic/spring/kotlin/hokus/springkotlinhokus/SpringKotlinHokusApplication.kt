d8aafbe11d86693ec40b9a464423f384d3343221
bbelovic <bbelovic@gmail.com>
2019-01-16 21:35:29 +0100

    Initial commit

---
+++ src/main/kotlin/org/bbelovic/spring/kotlin/hokus/springkotlinhokus/SpringKotlinHokusApplication.kt
@@ -0,0 +1,12 @@
+package org.bbelovic.spring.kotlin.hokus.springkotlinhokus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinHokusApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinHokusApplication>(*args)
+}
+
---
+++ src/test/kotlin/org/bbelovic/spring/kotlin/hokus/springkotlinhokus/SpringKotlinHokusApplicationTests.kt
@@ -0,0 +1,17 @@
+package org.bbelovic.spring.kotlin.hokus.springkotlinhokus
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringKotlinHokusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
