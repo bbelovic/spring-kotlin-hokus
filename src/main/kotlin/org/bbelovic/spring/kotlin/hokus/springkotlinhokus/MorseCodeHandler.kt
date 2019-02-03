d29773b393b313f4c33471e5fa2cff416a242d3f
bbelovic <bbelovic@gmail.com>
2019-02-03 15:26:18 +0100

    Change char to String for encode/decode table

--- src/main/kotlin/org/bbelovic/spring/kotlin/hokus/springkotlinhokus/MorseCodeHandler.kt
+++ src/main/kotlin/org/bbelovic/spring/kotlin/hokus/springkotlinhokus/MorseCodeHandler.kt
@@ -1,53 +1,53 @@
 package org.bbelovic.spring.kotlin.hokus.springkotlinhokus
 
 class MorseCodeHandler {
-    private val charToMorse = mapOf('A' to "._",
-            'B' to "_...",
-            'C' to "_._.",
-            'D' to "_..",
-            'E' to ".",
-            'F' to ".._.",
-            'G' to "__.",
-            'H' to "....",
-            'I' to "..",
-            'J' to ".___",
-            'K' to "_._",
-            'L' to "._..",
-            'M' to "__",
-            'N' to "_.",
-            'O' to "___",
-            'P' to ".__.",
-            'Q' to "__._",
-            'R' to "._.",
-            'S' to "...",
-            'T' to "_",
-            'U' to ".._",
-            'V' to "..._",
-            'W' to ".__",
-            'X' to "_.._",
-            'Y' to "_.__",
-            'Z' to "__..",
-            '1' to ".____",
-            '2' to "..___",
-            '3' to "...__",
-            '4' to "...._",
-            '5' to ".....",
-            '6' to "_....",
-            '7' to "__...",
-            '8' to "___..",
-            '9' to "____.",
-            '0' to "_____"
+    private val charToMorse = mapOf("A" to "._",
+            "B" to "_...",
+            "C" to "_._.",
+            "D" to "_..",
+            "E" to ".",
+            "F" to ".._.",
+            "G" to "__.",
+            "H" to "....",
+            "I" to "..",
+            "J" to ".___",
+            "K" to "_._",
+            "L" to "._..",
+            "M" to "__",
+            "N" to "_.",
+            "O" to "___",
+            "P" to ".__.",
+            "Q" to "__._",
+            "R" to "._.",
+            "S" to "...",
+            "T" to "_",
+            "U" to ".._",
+            "V" to "..._",
+            "W" to ".__",
+            "X" to "_.._",
+            "Y" to "_.__",
+            "Z" to "__..",
+            "1" to ".____",
+            "2" to "..___",
+            "3" to "...__",
+            "4" to "...._",
+            "5" to ".....",
+            "6" to "_....",
+            "7" to "__...",
+            "8" to "___..",
+            "9" to "____.",
+            "0" to "_____"
     )
 
     private val morseToChar = invert(charToMorse)
 
     fun encrypt(input: String): String {
         return input.asSequence().joinToString(separator = " ") {
-            charToMorse.getOrDefault(it, "")
+            charToMorse.getOrDefault(it.toString(), "")
         }
     }
 
-    private fun invert(m: Map<Char, CharSequence>): Map<CharSequence, Char> {
+    private fun invert(m: Map<String, String>): Map<CharSequence, CharSequence> {
         return m.asSequence().associate { entry -> entry.value to entry.key }
     }
 
